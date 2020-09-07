c7c655368e5fbe5bf52203c5744725bb5b7755a6
Ravi nntx <raviyadav5951@gmail.com>
2020-09-07 19:51:19 +0530

    completed weather map api integration

---
+++ WeatherProject/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const apiKey = "cc65d87afabccabcd3c47633ef7d504d";
+  const queryCity = req.body.cityName;
+  const units = "metric";
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    queryCity +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    units;
+
+  https.get(url, function (response) {
+    // console.log(response);
+    response.on("data", function (data) {
+      var weatherData = JSON.parse(data);
+      const weatherTem = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const imageCloud = weatherData.weather[0].icon;
+      const imageUrl =
+        "http://openweathermap.org/img/wn/" + imageCloud + "@2x.png";
+
+      res.write("<p>Weather in "+queryCity+" is " + description + "</p>");
+      res.write(
+        "<h1>The weather temperature in "+queryCity+" is " + weatherTem + " degree celsius</h1>"
+      );
+      res.write("<img src=" + imageUrl + ">");
+      res.send();
+    });
+  });
+
+});
+
+app.listen(3000, function () {
+  console.log("Listen on port 3000");
+});
